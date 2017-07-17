4ec3ac9495ee0e963e84aab56bbe278f9928e8b8
Sara Hamilton <Sara-Hamilton@users.noreply.github.com>
2017-07-17 15:17:59 -0700

    added login and signup pages

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -8,6 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:letsmingle@localh
 # user:password@server:portNumber/databaseName
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'dfh7q9DK1*d6'
 
 class Blog(db.Model):
 
@@ -32,57 +33,168 @@ class User(db.Model):
         self.password = password
 
 
-def validate_title(title):
-    title, title_error = title, ""
-    if title == "":
-        title_error = "Blog entry must have a title."
-    return title, title_error
+@app.route('/signup', methods=["POST", "GET"])
+def signup():
+    # render signup template
+    if request.method == "GET":
+        return render_template('signup.html', page_title="signup")
 
-def validate_body(body):
-    body, body_error = body, ""
-    if body == "":
-        body_error = "Blog entry must have content."
-    elif len(body) > 5000:
-        body_error = "Blog entry cannot be more than 5000 characters long."
-    return body, body_error
+    # define variables using form entries and database query
+    username = request.form["username"]
+    password = request.form["password"]
+    verify = request.form["verify"]
+    users = User.query.all()
 
+    # verification that username is filled in, is more than 3 characters long, and does not match a username in the database
+    if username == "":
+        username_error = "Name field cannot be blank"
+    elif len(username) <= 3:
+        username_error = "Name must be at least four characters long"
+    elif str(username) in str(users):
+        username_error = "That username already exists.  Enter a different username."
+        # TODO - make this work - make user name unique
+    else:        
+        username_error = ""
+
+    # verification that password is filled in and is more than 3 characters long
+    if password == "":
+        password_error = "Password field cannot be blank"
+    elif len(password) <= 3:
+        password_error = "Password must be at least four characters long"
+    else:
+        password_error = ""
+
+    # verification that verify password field is filled in and matches password
+    if verify == "":
+        verify_error = "Verify Password field cannot be blank"
+    elif verify != password:
+        verify_error = "Passwords do not match"
+    else:
+        verify_error = ""
+
+    # if there are no errors, adds new user to the database, creates a new session, and redirects user to /newpost template
+    if not username_error and not password_error and not verify_error:
+        if request.method == "POST":
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            return redirect('/newpost')
+    else:
+        # re-renders signup template with appropriate error messages if errors exist
+        return render_template('signup.html', page_title = "signup", username = username, username_error = username_error, 
+            password_error = password_error, verify_error = verify_error)
+    
+
+@app.route('/login', methods=["POST", "GET"])
+def login(): 
+
+    # render login template
+    if request.method == "GET":
+        return render_template('login.html', page_title="login")
+
+    # define variables using form entries and database queries
+    username = request.form["username"]
+    password = request.form["password"]
+    users = User.query.all()
+    user = User.query.filter_by(username=username).first() 
+
+    # verification that user name is filled in and matches a user name in the database
+    if username == "":
+        username_error = "name field cannot be blank"
+    elif str(username) not in str(users):
+        username_error = "user name not registered"
+        # TODO - make this work
+    else:        
+        username_error = ""
+
+    # verification that password is filled in and matches the password for the given user in the database
+    if password == "" and str(username) in str(users):
+        password_error = "password cannot be blank" 
+    # TODO - fix this so that it works - elif password != user.password:
+        password_error = "incorrect password"
+    else:
+        password_error = ""
+    
+    # if there are no errors, creates new session and redirects user to newpost template
+    if username_error == "" and  password_error == "":
+        if request.method == 'POST':
+            if user and user.password == password:
+                session['username'] = username
+                flash("Logged in")
+                return redirect('/newpost') 
+
+    # re-renders login template with appropriate error messages if login errors exist
+    else:
+        return render_template('login.html', page_title = "login", username = username, username_error = username_error, password_error = password_error)
+
+
+'''
+@app.route('/index', methods=["POST", "GET"])
+def index():
+    # TODO - create
+
+@app.route('/logout', methods=["POST"])
+def logout():
+    # TODO - create - Handles a POST request to /logout and redirects the user to /blog after deleting the user name from the session.
+'''
 @app.route('/blog', methods=["POST", "GET"])
 def index():
+    # define variable using database query
     entries = Blog.query.all()
     
+    # redirects to single blog entry when blog title is clicked
     if "id" in request.args:
         id = request.args.get('id')
         entry = Blog.query.get(id)
         
         return render_template('entries.html', page_title="blog-post", title = entry.title, body = entry.body)
-    
+
+    # displays template posts which displays all entries in descending order
     return render_template('posts.html', page_title="blog", entries = entries)
 
 
 @app.route('/newpost', methods=["POST", "GET"])
 def add_entry():
-  
-    return render_template('newpost.html', page_title="blog")
-
-
-@app.route('/validate', methods=["POST", "GET"])
-def validate():
-    title, title_error = validate_title(request.form["title"])
-    body, body_error = validate_body(request.form["body"])
     
+    # render newpost template
+    if request.method == "GET":
+        return render_template('newpost.html', page_title="blog")
+
+    # define variables using form entries
+    title = request.form["title"]
+    body = request.form["body"]
+
+    # verification that title is filled in
+    if title == "":
+        title_error = "Blog entry must have a title."
+    else:
+        title_error = "" 
+
+    # verification that body is filled in
+    body = request.form["body"]
+    if body == "":
+        body_error = "Blog entry must have content."
+    elif len(body) > 5000:
+        body_error = "Blog entry cannot be more than 5000 characters long."
+    else:
+        body_error = ""
+    
+    # if there are no errors, adds new entry to the database and redirects user to entries template
     if not title_error and not body_error:
         if request.method == "POST":
-            title_name = request.form['title']
-            body_name = request.form['body']
-            # owner_name = (fill this in)
-            new_entry = Blog(title_name, body_name) # add owner_name to this list
+            # title_name = request.form["title"] # Is this needed?
+            # body_name = request.form["body"]
+            owner = User.query.filter_by(username =session['username']).first()
+            new_entry = Blog(title_name, body_name, owner) 
             db.session.add(new_entry)
             db.session.commit()
         
         return render_template('entries.html', page_title = "confirmation", title = title, body = body)
     else:
-        return render_template('newpost.html', page_title = "blog", title = title, title_error = title_error, body = body, body_error = body_error)
-
+        # re-renders newpost template with appropriate error messages if errors exist
+        return render_template('newpost.html', page_title = "blog", title = title, 
+            title_error = title_error, body = body, body_error = body_error)
 
 if __name__ == '__main__':
     app.run()
